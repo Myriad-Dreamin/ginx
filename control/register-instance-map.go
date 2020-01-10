9fb594bccd6aa1ae08467a6aa60cd635da9796f4
Myriad-Dreamin <camiyoru@gmail.com>
2020-01-10 22:41:58 +0800

    delete somefiles

--- control/register-instance-map.go
+++
@@ -1,8 +0,0 @@
-// Code generated minimum-attach-file.go DO NOT EDIT
-package control
-
-import instance "github.com/Myriad-Dreamin/go-magic-package/instance"
-
-func init() {
-	instance.Register("github.com/Myriad-Dreamin/minimum-template/control", `/home/kamiyoru/work/gosrc/src/github.com/Myriad-Dreamin/minimum-template/control`)
-}
--- init-package-probe.go
+++
@@ -1,6 +0,0 @@
-// Code generated attach-file.go DO NOT EDIT
-package main
-
-import (
-	_ "github.com/Myriad-Dreamin/minimum-template/package-probe"
-)
